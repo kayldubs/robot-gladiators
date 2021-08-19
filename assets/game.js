c68316d639cb152fa2c4bfcac592a803d5f7e9df
kayldubs <kstevensgraphicarts@gmail.com>
2021-08-19 09:28:44 -0500

    for loop

--- assets/game.js
+++ assets/game.js
@@ -3,11 +3,16 @@ var playerHealth = 100;
 var playerAttack = 10;
 var playerMoney = 10;
 
-var enemyName = "Roborto";
+var enemyNames = ["Roborto", "Amy Andriod", "Robo Trumble"];
 var enemyHealth = 50;
 var enemyAttack = 12;
+//for(var i = 0; i < enemyNames.length; i++) {
+    //console.log(enemyNames[i]);
+    //console.log(i);
+    //console.log(enemyNames[i] + " is at" + i + " index");
+//}
 
-var fight = function() {
+var fight = function(enemyName) {
     window.alert("Welcome to Robot Gladiators!");
     var promptFight = window.prompt('Would you like to FIGHT or SKIP this battle? Enter "FIGHT" or "SKIP" to choose.'); 
 if (promptFight === "fight" || promptFight === "FIGHT")
@@ -50,4 +55,6 @@ else {
     window.alert("You need to choose a valid option. Try again!");
 }
 };
-fight();
+for(var i = 0; i < enemyNames.length; i++) {
+    fight(enemyNames[i]);
+}
